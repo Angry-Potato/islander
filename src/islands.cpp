d20146be9c4ef2cf65b57e39a5c3db72fc8c4a61
Angry-Potato <liam.humphreys1989@gmail.com>
2017-08-08 20:56:42 +0100

    new thoughts on algorithm

--- src/islands.cpp
+++ src/islands.cpp
@@ -11,12 +11,35 @@ Place* Islands::find(std::forward_list<Place*>& places) {
   }
   /*
   Place* potentialIsland
+  Place* nextPotentialIsland
   for each place in places
     if (potentialIsland == NULL) {
       potentialIsland = place
     }
+    if (potentialIsland->nearestPlace == NULL) {
+      potentialIsland->nearestPlace = Places::nearest(potentialIsland, places)
+    }
+    nextPotentialIsland = nextPotentialIsland(potentialIsland, places)
+    if (nextPotentialIsland == potentialIsland) {
+      return potentialIsland
+    }
 
 
+
+
+    function nextPotentialIsland::
+      for each innerloopplace in places 
+        if (innerloopplace->nearestPlace == NULL) {
+          innerloopplace->nearestPlace = Places::nearest(innerloopplace, places)
+        }
+        if innerloopplace->distToNearestPlace < potentialIsland->distToNearestPlace
+          innerloopplace->isPotentialIsland = false
+          innerloopplace->nearestPlace->isPotentialIsland = false
+        else
+          potentialIsland->isPotentialIsland = false
+          potentialIsland->nearestPlace->isPotentialIsland = false
+          return innerloopplace
+        return potentialIsland
 
 
   */
